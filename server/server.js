43fe1cc4d3f672c3862fa57394a307713884e43f
FranciscoGJ <francisco.gonzalezj@mail.udp.cl>
2017-10-20 03:27:33 -0300

    server up

---
+++ server/server.js
@@ -0,0 +1,17 @@
+var PORT = 1700;
+var HOST = '::';
+
+var dgram = require('dgram');
+var server = dgram.createSocket('udp6');
+
+server.on('listening', function () {
+    var address = server.address();
+    console.log('UDP Server listening on ' + address.address + ":" + address.port);
+});
+
+server.on('message', function (message, remote) {
+    console.log(remote.address + ':' + remote.port +' - ' + message);
+
+});
+
+server.bind(PORT, HOST);
